0d2a950c2799c7f7e46f81c3a1c6c6f1d092d2f3
Enea Xharja <19818937+eneax@users.noreply.github.com>
2021-06-19 17:44:50 +0200

    fs.appendFileSync()

--- app.js
+++ app.js
@@ -8,4 +8,13 @@
 
 const fs = require('fs')
 
-fs.writeFileSync('notes.txt', 'My name is Enea.')
+// fs.writeFileSync('notes.txt', 'My name is Enea.')
+
+//
+// Challenge: Append a message to notes.txt
+//
+// 1. Use appendFileSync to append to the file
+// 2. Run the script
+// 3. Check your work by opening the file and viewing the appended text
+
+fs.appendFileSync('notes.txt', '\nMy last name is Xharja.')
