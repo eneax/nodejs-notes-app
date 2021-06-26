3e1ed50e783e324c2a58fbed9a2a41ee8224a53f
Enea Xharja <19818937+eneax@users.noreply.github.com>
2021-06-26 15:15:55 +0200

    Remove comments

--- app.js
+++ app.js
@@ -4,16 +4,6 @@ const getNotes = require('./notes')
 const msg = getNotes()
 console.log(msg)
 
-//
-// Use the chalk library in your project
-//
-// 1. Install the version 2.4.1 of chalk
-// 2. Load chalk into your app
-// 3. Use it to print the string "Success!" to the console in green
-// 4. Test your work
-
-// Bonus: Use the docs to mess around with other styles. Make text bold and inverted.
-
 const greenMsg = chalk.green('Success!')
 console.log('greenMsg', greenMsg)
 
