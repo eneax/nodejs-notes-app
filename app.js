1476b2eb216cc5cb2c7860d4ea05f841a6d2d858
Enea Xharja <19818937+eneax@users.noreply.github.com>
2021-06-26 14:45:17 +0200

    Try the validator package

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+const validator = require('validator')
+
 const getNotes = require('./notes')
 
 const msg = getNotes()
-
 console.log(msg)
+
+console.log(validator.isEmail('enea@example.com'))
+console.log(validator.isEmail('@example.com'))
+
+console.log(validator.isURL('https://eneaxharja.com'))
+console.log(validator.isURL('https/eneaxharja.com'))
