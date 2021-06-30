4083ff1f6ec71b20e58643598a736fc2207bb69b
Enea Xharja <19818937+eneax@users.noreply.github.com>
2021-06-30 21:46:55 +0200

    argv example

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 const chalk = require('chalk')
 const getNotes = require('./notes')
 
-/*
-! argv
-- it stays for argument vector
-- in case of Node.js it's an array that contains all the arguments provided
-- the array contains 3 strings and 2 of them are always provided
-- the first string is the path to the Node.js executable on your machine
-- the second string is that path to the app.js file 
-- the third string is the value that we actually provided when running `node app.js Enea`
-*/
+const command = process.argv[2]
 
-console.log(process.argv)
-console.log('name', process.argv[2])
+if (command === 'add') {
+  console.log('Adding note!')
+} else if (command === 'remove') {
+  console.log('Removing note!')
+}
