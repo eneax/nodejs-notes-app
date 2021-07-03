003bb44f08790c1520fc2ebb1770b421e41cf35f
Enea Xharja <19818937+eneax@users.noreply.github.com>
2021-07-03 15:07:03 +0200

    Argument parsing with Yargs

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
+
 const getNotes = require('./notes')
 
-const command = process.argv[2]
+// Customize yargs version
+yargs.version('1.1.0')
 
-if (command === 'add') {
-  console.log('Adding note!')
-} else if (command === 'remove') {
-  console.log('Removing note!')
-}
+console.log(yargs.argv)
