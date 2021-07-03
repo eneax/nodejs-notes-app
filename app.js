bf83108b57a9b11904a35b6a8b4aa24f85640ad5
Enea Xharja <19818937+eneax@users.noreply.github.com>
2021-07-03 15:24:24 +0200

    list and read commands with Yargs

--- app.js
+++ app.js
@@ -24,4 +24,22 @@ yargs.command({
   },
 })
 
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List your notes',
+  handler: function () {
+    console.log('Listing out all notes!')
+  },
+})
+
+// Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: function () {
+    console.log('Reading a note!')
+  },
+})
+
 console.log(yargs.argv)
