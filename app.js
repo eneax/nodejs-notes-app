b9720b00440a3a5e411156c9bcebd4b374eda6d6
Enea Xharja <19818937+eneax@users.noreply.github.com>
2021-07-03 15:16:44 +0200

    add and remove commands with Yargs

--- app.js
+++ app.js
@@ -6,4 +6,22 @@ const getNotes = require('./notes')
 // Customize yargs version
 yargs.version('1.1.0')
 
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: function () {
+    console.log('Adding a new note!')
+  },
+})
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function () {
+    console.log('Removing a note!')
+  },
+})
+
 console.log(yargs.argv)
