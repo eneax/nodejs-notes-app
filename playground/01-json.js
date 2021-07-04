1211ba0dbe474a47ab1d8160f0eeefbfed565be4
Enea Xharja <19818937+eneax@users.noreply.github.com>
2021-07-04 10:35:20 +0200

    Read and write JSON

--- playground/01-json.js
+++ playground/01-json.js
@@ -2,10 +2,28 @@
 const fs = require('fs')
 
 // JS object
-const book = {
-  title: 'Sapiens',
-  author: 'Yuval Noah Harari',
-}
+// const book = {
+//   title: 'Sapiens',
+//   author: 'Yuval Noah Harari',
+// }
 
-const bookJSON = JSON.stringify(book)
-fs.writeFileSync('playground/01-json.json', bookJSON)
+// Convert JS object into JSON string
+// const bookJSON = JSON.stringify(book)
+
+// Save data into JSON file
+// fs.writeFileSync('playground/01-json.json', bookJSON)
+
+// Read file
+const dataBuffer = fs.readFileSync('playground/01-json.json')
+
+// Buffer is a way for NodeJS to represent binary data
+// Meaning that dataBuffer looks completely different from bookJSON
+// console.log(dataBuffer)
+
+// Convert buffer to string
+const dataJSON = dataBuffer.toString()
+// console.log(dataJSON)
+
+// Convert JSON string into JS object
+const data = JSON.parse(dataJSON)
+console.log(data)
