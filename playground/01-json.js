137896eca29d82946be5f681db8e254a7e96e1f8
Enea Xharja <19818937+eneax@users.noreply.github.com>
2021-07-04 10:44:39 +0200

    Cleanup and add challenge instructions

--- playground/01-json.js
+++ playground/01-json.js
@@ -14,16 +14,23 @@ const fs = require('fs')
 // fs.writeFileSync('playground/01-json.json', bookJSON)
 
 // Read file
-const dataBuffer = fs.readFileSync('playground/01-json.json')
+// const dataBuffer = fs.readFileSync('playground/01-json.json')
 
 // Buffer is a way for NodeJS to represent binary data
 // Meaning that dataBuffer looks completely different from bookJSON
 // console.log(dataBuffer)
 
 // Convert buffer to string
-const dataJSON = dataBuffer.toString()
+// const dataJSON = dataBuffer.toString()
 // console.log(dataJSON)
 
 // Convert JSON string into JS object
-const data = JSON.parse(dataJSON)
-console.log(data)
+// const data = JSON.parse(dataJSON)
+// console.log(data)
+
+// TODO: Challenge Time - Work with JSON and the file system
+
+// 1. Load and parse the JSON data
+// 2. Change the name and age property using your info
+// 3. Stringify the changed object and overwrite the original data
+// 4. Test your work by viewing data in the JSON file
